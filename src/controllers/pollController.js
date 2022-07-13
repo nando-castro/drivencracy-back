dbc9bba2ca0c37614f68e8265b46e9ebd42fff5a
nando-castro <fernando.castro10.master@gmail.com>
2022-07-13 14:05:18 -0300

    poll create finish

--- src/controllers/pollController.js
+++ src/controllers/pollController.js
@@ -1,12 +1,14 @@
 import joi from "joi";
 import { db } from "../db/mongodb.js";
 import dotenv from "dotenv";
+import daysjs from "dayjs";
+import dayjs from "dayjs";
 dotenv.config();
 
 export async function registerPoll(req, res) {
   const registerSchema = joi.object({
     title: joi.string().min(1).required(),
-    expireAt: joi.date().greater("now"),
+    expireAt: joi.date().min("now"),
   });
 
   const poll = req.body;
@@ -14,21 +16,30 @@ export async function registerPoll(req, res) {
   const { error } = registerSchema.validate(poll);
 
   if (error) {
-    return res.status(422).send("Não está no formato correto");
+    return res.status(422).send(`"Não está no formato correto" ${error}`);
   }
 
-  if (poll.expireAt === "") {
-    const time = new Date();
-    const outraData = new Date();
-    outraData.setDate(time.getDate() + 3);
+  try {
+    const pollExists = await db
+      .collection("polls")
+      .findOne({ title: poll.title });
 
-    Date.prototype.addDays = function (days) {
-      this.setDate(this.getDate() + days);
-    };
+    if (pollExists) {
+      return res.status(401).send("Poll exists!!");
+    }
+  } catch (error) {
+    res.sendStatus(500);
+  }
 
-    const dt = new Date();
+  if (!poll.expireAt || poll.expireAt === "") {
+    const dt = daysjs().add(30, "day").format("YYYY-MM-DD 23:59");
 
-    poll.expireAt === dt;
+    await db.collection("polls").insertOne({
+      title: poll.title,
+      expireAt: dt,
+    });
+
+    return res.status(201).send("data add automaticamente");
   }
   try {
     await db.collection("polls").insertOne({
